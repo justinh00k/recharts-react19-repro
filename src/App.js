2d0f5fcc5076c21bc0435b5d059ccfe58d85ae51
justin hook <justinhook@gmail.com>
2024-05-20 13:21:05 -0700

    upgrade to react@beta, react-dom@beta

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import { Bar, BarChart, ResponsiveContainer, XAxis, YAxis } from 'recharts';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				Chart:
+				<ResponsiveContainer height={100} width={'100%'}>
+					<BarChart
+						data={[
+							{ name: 'a', value: 10 },
+							{ name: 'b', value: 20 },
+						]}>
+						<XAxis dataKey="name" />
+						<YAxis dataKey="value" />
+						<Bar dataKey="value" />
+					</BarChart>
+				</ResponsiveContainer>
+			</header>
+		</div>
+	);
 }
 
 export default App;
